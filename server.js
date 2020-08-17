19525f43e92f79b1c3cb57b8be96e515f8319924
hilelley <hilelley@gmail.com>
2020-08-17 13:20:29 +0300

    add product -is work

--- server.js
+++ server.js
@@ -1,45 +1,55 @@
 const express = require("express");
-const fs = require("fs");
 const app = express();
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
-app.use(bodyParser.json());
-
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const path = require("path");
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+dotenv.config();
 app.use(cors());
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+app.use(morgan("combined", { stream: accessLogStream }));
+const PORT = process.env.PORT ? process.env.PORT : 8000;
+const JSON_FILE = process.env.JSON_FILE ? process.env.JSON_FILE : JSON_FILE;
 
 app.get("/products", (req, res) => {
-  console.log("QUERY:", req.query.search);
   const search = req.query.search;
-  fs.readFile("products.json", (err, data) => {
+  const query = req.query;
+  fs.readFile(JSON_FILE, (err, data) => {
     products = JSON.parse(data);
     if (search) {
       const filteredproducts = products.filter((product) =>
         product.title.includes(search)
       );
       res.send(filteredproducts);
+      // } else if (JSON.stringify(query) === JSON.stringify({ search: "" })) {
+      //   res.send([]);
     } else {
       res.send(products);
-      console.log(products);
     }
   });
 });
 
 app.post("/products", (req, res) => {
-  fs.readFile("products.json", (err, data) => {
+  fs.readFile(JSON_FILE, (err, data) => {
     const products = JSON.parse(data);
     const product = req.body;
 
     products.push(product);
-    fs.writeFile("products.json", JSON.stringify(products), (err) => {
-      // console.log(err);
+    fs.writeFile(JSON_FILE, JSON.stringify(products), (err) => {
       res.send("YOU SUCCEED!!!");
     });
   });
 });
 
 app.delete("/products/:id", (req, res) => {
-  fs.readFile("products.json", (err, data) => {
+  fs.readFile(JSON_FILE, (err, data) => {
     const products = JSON.parse(data);
     const productId = +req.params.id;
     const productIndex = products.findIndex(
@@ -47,28 +57,43 @@ app.delete("/products/:id", (req, res) => {
     );
     console.log(productIndex);
     products.splice(productIndex, 1);
-    fs.writeFile("products.json", JSON.stringify(products), (err) => {
+    fs.writeFile(JSON_FILE, JSON.stringify(products), (err) => {
       res.send("YOU SUCCEED!!!");
     });
   });
 });
 
 app.put("/products/:id", (req, res) => {
-  fs.readFile("products.json", (err, data) => {
-    const products = JSON.parse(data);
-    const productId = +req.params.id;
-    const productIndex = products.findIndex(
-      (product) => product.id === productId
-    );
-    console.log(req.body);
-    const ceng = req.body;
-    products[productIndex] = { ...products[productIndex], ...ceng };
-    fs.writeFile("products.json", JSON.stringify(products), (err) => {
-      res.send("YOU SUCCEED!!!");
+  const params = req.params.id;
+  if (params === "add") {
+    fs.readFile(JSON_FILE, (err, data) => {
+      let products = JSON.parse(data);
+      console.log(req.body);
+      let newProduct = req.body;
+      newProduct = { id: products.length + 1, ...newProduct };
+      console.log(newProduct);
+      products = [...products, newProduct];
+      fs.writeFile(JSON_FILE, JSON.stringify(products), (err) => {
+        res.send("YOU SUCCEED!!!");
+      });
     });
-  });
+  } else {
+    fs.readFile(JSON_FILE, (err, data) => {
+      const products = JSON.parse(data);
+      const productId = +req.params.id;
+      const productIndex = products.findIndex(
+        (product) => product.id === productId
+      );
+      console.log(req.body);
+      const cheng = req.body;
+      products[productIndex] = { ...products[productIndex], ...cheng };
+      fs.writeFile(JSON_FILE, JSON.stringify(products), (err) => {
+        res.send("YOU SUCCEED!!!");
+      });
+    });
+  }
 });
 
-app.listen(8000, () => {
-  console.log("Example app listening on port 8000!");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
